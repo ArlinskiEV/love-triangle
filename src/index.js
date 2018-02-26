d1febfd0d3ce04ffc1eeca48b8412ef4d1746a5d
ArlinskiEV <ArlinskiEV@gmail.com>
2018-02-26 14:48:57 +0300

    love-triangle/features: simple

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let arr = preferences.map((item) => (item - 1));
+  let count = 0;
+  let checked = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr[arr[arr[i]]]) {
+      let triangle = [i, arr[i], arr[arr[i]]].sort().join('-');
+      if (checked.indexOf(triangle) === -1) {
+        checked.push(triangle);
+        count += 1;
+      }
+    }
+  }
+
+  return count;
 };
